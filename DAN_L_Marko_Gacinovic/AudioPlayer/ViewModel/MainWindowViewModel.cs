0119dbd876ff4587e7377c65243737054d5f53c9
MarkoG22 <marko.gacinovic.el@gmail.com>
2020-08-05 13:46:44 +0200

    UserMenu View and ViewModel

---
+++ DAN_L_Marko_Gacinovic/AudioPlayer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,116 @@
+﻿using AudioPlayer.Commands;
+using AudioPlayer.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AudioPlayer.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        MainWindow main;
+
+        // properties for username and password
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        // constructor
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        // command for the login button
+        private ICommand logIn;
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => SaveExecute(), param => CanSaveExecute());
+                }
+                return logIn;
+            }
+        }
+
+        private bool CanSaveExecute()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// method for checking username and password and opening the windows
+        /// </summary>
+        private void SaveExecute()
+        {
+            if (password.Length >= 6)
+            {
+                UserMenu user = new UserMenu();
+                user.ShowDialog();
+            }            
+            else
+            {
+                MessageBox.Show("Wrong username or password, please try again (Password minimum 6 characters)");
+            }
+        }
+
+        // command for closing the window
+        private ICommand logout;
+        public ICommand Logout
+        {
+            get
+            {
+                if (logout == null)
+                {
+                    logout = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return logout;
+            }
+        }
+
+        /// <summary>
+        /// method for closing the window
+        /// </summary>
+        private void CloseExecute()
+        {
+            try
+            {
+                main.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+            }
+        }
+
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+    }
+}
